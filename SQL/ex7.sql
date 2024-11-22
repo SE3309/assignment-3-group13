0d527915de9614b155fff6420def3bf336fd316b
lshyam <lshyam@uwo.ca>
2024-11-21 19:56:37 -0500

    added ex7 file

---
+++ SQL/ex7.sql
@@ -0,0 +1,33 @@
+CREATE VIEW HighExpenses AS
+SELECT 
+    u.UserID,
+    u.Username,
+    e.ExpenseCategory,
+    e.TotalExpense
+FROM 
+    User u
+JOIN 
+    Expense e ON u.UserID = e.UserID
+WHERE 
+    e.TotalExpense > 5000;
+
+
+
+SELECT * FROM HighExpenses WHERE ExpenseCategory = 'Travel';
+
+
+CREATE VIEW IncomeSummary AS
+SELECT 
+    u.UserID,
+    u.Username,
+    SUM(i.TotalIncome) AS TotalIncome
+FROM 
+    User u
+JOIN 
+    Income i ON u.UserID = i.UserID
+GROUP BY 
+    u.UserID, u.Username;
+
+
+
+SELECT * FROM IncomeSummary WHERE TotalIncome > 20000;
